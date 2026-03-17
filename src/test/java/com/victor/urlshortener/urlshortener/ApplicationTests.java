e36998ec4902d6a93b28375c0ad72eae13e1eae6
Victor Americo <victoroeste12@gmail.com>
2026-03-17 13:34:35 -0300

    subindo e configurando o projeto

---
+++ src/test/java/com/victor/urlshortener/urlshortener/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.urlshortener.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
